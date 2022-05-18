b8ef300f160f8331619e599f1e5280632bdba8d9
Rahman <abdurrahman1270@gmail.com>
2022-05-18 21:46:00 +0700

    Logic to get Crystal

---
+++ Assets/Scripts/CollectCrystals.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCrystals : MonoBehaviour
+{
+
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            collectSound.Play();
+            ScoringSystem.collectedCrystals++;
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int collectedCrystals;
+    public int totalCrystals;
+
+    void Update()
+    {
+        scoreText.GetComponent<TextMeshProUGUI>().text = "Crystals: " + collectedCrystals + "/" + totalCrystals;
+    }
+}
