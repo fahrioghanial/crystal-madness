b3d55894bff270862aa4dbce6bf16aa9e69ab068
fahrioghanial <fahrioghanial@gmail.com>
2022-06-01 17:24:10 +0700

    fixing

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject player;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+    public Texture2D cursorTexture;
+    public Vector2 hotSpot = Vector2.zero;
+
     // Update is called once per frame
     void Update()
     {
@@ -26,22 +32,30 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        //player.GetComponent<FirstPersonController>().lockCursor();
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        GameIsPaused = false;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        GameIsPaused = false;
     }
 
     void Pause()
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        player.GetComponent<FirstPersonController>().unlockCursor();
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         GameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; 
     }
 
     public void LoadMenu()
     {
-        SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1f;
         GameIsPaused = false;
+        SceneManager.LoadScene("MainMenu");
     }
 }
--- Assets/Scripts/StageCompleteTrigger.cs
+++ Assets/Scripts/StageCompleteTrigger.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Collections;
 
 public class StageCompleteTrigger : MonoBehaviour
 {
-    void OnTriggerEnter()
+    public GameObject warningUI;
+
+    IEnumerator OnTriggerEnter()
     {
+       
+
         if (ScoringSystem.isAllCrystalCollected)
         {
             FindObjectOfType<GameManager>().SetStageCompleted();
         }
+        else
+        {
+            GetComponent<Collider>().isTrigger = false;
+            warningUI.SetActive(true);
+            yield return new WaitForSeconds(5);
+            warningUI.SetActive(false);
+            GetComponent<Collider>().isTrigger = true;
+        }
     }
 }
+
